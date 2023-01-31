6a0f2550926bd22f8395011298f0175b8b18416a
Derrick Fleming <derrick.fleming7@gmail.com>
2023-01-31 23:48:18 +0000

    Create post route to server and update database schmeas.

--- server/index.js
+++ server/index.js
@@ -2,6 +2,14 @@ require('dotenv/config');
 const express = require('express');
 const staticMiddleware = require('./static-middleware');
 const errorMiddleware = require('./error-middleware');
+const pg = require('pg');
+
+const db = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
 const app = express();
 
@@ -9,6 +17,32 @@ app.use(staticMiddleware);
 
 app.get('/api/hello', (req, res) => {
   res.json({ hello: 'world' });
+});
+
+app.post('/api/todos', async (req, res) => {
+  try {
+    const { task, isCompleted = false } = req.body;
+    if (!task || typeof isCompleted !== 'boolean') {
+      res.status(400).json({
+        error: 'task (string) and isCompleted (boolean) are required fields'
+      });
+      return;
+    }
+    const sql = `
+    insert into "todos" ("task", "isCompleted")
+    values ($1, $2)
+    returning *`;
+
+    const params = [task, isCompleted];
+    const dbresult = await db.query(sql, params);
+    const [todo] = dbresult.rows;
+    res.status(201).json(todo);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({
+      error: 'an unexpected error occcurred'
+    });
+  }
 });
 
 app.use(errorMiddleware);
