e5c89743459af2d0110a990bd5970b6a22b39f5f
Derrick Fleming <derrick.fleming7@gmail.com>
2023-02-06 00:51:22 +0000

    Style todo-list component with new todoItem component that contains checkbox and label. Resize form components.

--- client/components/todo-list.tsx
+++ client/components/todo-list.tsx
@@ -3,12 +3,31 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import { useDispatch, useSelector } from "react-redux";
 import { getTodosAsync } from '../redux/todos-slice';
+import Form from 'react-bootstrap/Form';
 
 type Todos = {
   task: string,
   isCompleted: boolean,
   todoId: string
 };
+
+
+
+function TodoItem(props: { todo: { todoId: string, task: string, isCompleted: boolean } }) {
+  const { todoId, task, isCompleted } = props.todo;
+  const idAttr = `todo-item-${todoId}`;
+  const taskClass = isCompleted
+    ? 'is-completed'
+    : '';
+  return (
+    <li>
+      <div>
+        <Form.Check type="checkbox" id={idAttr} label={task} className={`${taskClass} fs-4 py-2 px-2 border-bottom border-2 border-light`}></Form.Check>
+      </div>
+    </li>
+  )
+}
+
 
 export default function TodoList() {
   const todos = useSelector( (state:{ todosList: Todos[]}) => state.todosList);
@@ -22,15 +41,15 @@ export default function TodoList() {
   const list = todos.length === 0 ? 'Loading...' :
     todos.map(todo => {
       return (
-        <li key={todo.todoId}>
-          <h1>{todo.task}</h1>
-        </li>
+        <TodoItem
+        key={todo.todoId}
+        todo={todo} />
       )
     });
 
   return (
-    <Row>
-      <Col>
+    <Row className="justify-content-center">
+      <Col md={8} xs={11}>
         <ul>
           {list}
         </ul>
