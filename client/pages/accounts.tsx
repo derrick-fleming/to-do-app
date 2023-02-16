4b91a4ca14572307be0dd7f53d35fb0635f3a22e
Derrick Fleming <derrick.fleming7@gmail.com>
2023-02-16 01:59:33 +0000

    Created a navigation bar to move between different pages. Added a login component file.

---
+++ client/pages/accounts.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
+
+export default function LoginPage () {
+  return (
+    <Row>
+      <Col>
+        <Form>
+
+        </Form>
+      </Col>
+    </Row>
+  )
+}
--- client/pages/home.tsx
+++ client/pages/home.tsx
@@ -2,13 +2,33 @@ import React from 'react';
 import TodoForm from '../components/todo-form';
 import { Container } from 'react-bootstrap';
 import TodoList from '../components/todo-list';
+import LoginPage from './accounts';
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 
 export default function Home() {
   return (
-    <Container>
-      <h1 className="text-center my-4">My Todo List</h1>
-      <TodoForm />
-      <TodoList />
-    </Container>
+    <>
+      <Navbar collapseOnSelect expand="md" bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Todos</Navbar.Brand>
+          <Navbar.Toggle aria-controls="response-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav>
+              <Nav.Link href="#home">My List</Nav.Link>
+              <Nav.Link href="#login">Login</Nav.Link>
+              <Nav.Link href="#logout">Logout</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <Container>
+        <h1 className="text-center my-4">My Todo List</h1>
+        <LoginPage />
+        <TodoForm />
+        <TodoList />
+      </Container>
+    </>
+
   );
 }
