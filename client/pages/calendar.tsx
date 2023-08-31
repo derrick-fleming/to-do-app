1357ae7cc27069abdc36975e768285f710314e1f
Derrick Fleming <derrick.fleming7@gmail.com>
2023-08-31 22:45:13 +0000

    Adds default value and styling to calendar when user clicks on a specific date. Loads today's date as default value.

--- client/pages/calendar.tsx
+++ client/pages/calendar.tsx
@@ -1,13 +1,18 @@
-import React from "react"
+import React, { useState } from "react"
 import Container from "react-bootstrap/Container";
 import Calendar from "react-calendar";
 
+type ValuePiece = Date | null;
+
+type Value = ValuePiece | [ValuePiece, ValuePiece];
+
 export default function CalendarComponent() {
+  const [value, onChange] = useState<Value>(new Date());
 
   return (
     <Container>
         <h1 className="text-center my-4">Calendar</h1>
-        <Calendar className="calendar-styles p-4"/>
+        <Calendar onChange={onChange} value={value} className="calendar-styles p-4"/>
     </Container >
 
   )
